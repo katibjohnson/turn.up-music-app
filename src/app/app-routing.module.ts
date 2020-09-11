19892715cfb51a52ba5c86ea2e38ba486b5f9647
Chris <chrisshaohualiu@gmail.com>
2020-09-11 10:30:55 -0400

    added components/paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArtistComponent } from './artist/artist.component';
+import { HomeComponent } from './home/home.component';
+import { ResultsComponent } from './results/results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'home', component: HomeComponent },
+  { path: 'results', component: ResultsComponent },
+  { path: 'artist', component: ArtistComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: '**', component: NotFoundComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ResultsComponent } from './results/results.component';
+import { ArtistComponent } from './artist/artist.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ResultsComponent,
+    ArtistComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
