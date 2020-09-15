9eda913f49abc8bdc4ed0650eb203ed0d5652072
Chris <chrisshaohualiu@gmail.com>
2020-09-15 11:07:51 -0400

    contacted api thru results page

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LastFmService } from '../last-fm.service';
 
 @Component({
   selector: 'app-results',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent implements OnInit {
+  searchResults: any = [];
 
-  constructor() { }
+  constructor(private lastFm: LastFmService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getSearchResults();
   }
+
+  getSearchResults= ()=>{
+    this.route.queryParamMap.subscribe((params)=>{
+      this.lastFm.getArtists(params.get('artist')).subscribe((response)=>{
+        this.searchResults = response.results.artistmatches.artist;
+        console.log(this.searchResults);
+      })
+    })
+  }
+
 
 }
