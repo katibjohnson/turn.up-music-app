ea90517aacf4b1cce173b8712a8c9c41469a3f11
Chris <chrisshaohualiu@gmail.com>
2020-09-16 13:36:39 -0400

    favorites page started

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LastFmService } from '../last-fm.service';
+import { TurnUpService } from '../turn-up.service';
 
 @Component({
   selector: 'app-favorites',
@@ -7,7 +9,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./favorites.component.css'],
 })
 export class FavoritesComponent implements OnInit {
-  constructor(public router: Router) {}
+  constructor(public router: Router, private lastFm: LastFmService, private turnup: TurnUpService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getFavoriteArtists();
+  }
+  favoritesList: any = [];
+  getFavoriteArtists = ()=>{
+    this.turnup.getFavoriteArtists().subscribe((response)=>{
+      this.favoritesList = response;
+    })
+  }
 }
