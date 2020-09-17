9f89fa3af5f8c1559e94933f0ceef63ff565924a
kati <katibrooke.j@gmail.com>
2020-09-17 15:02:01 -0400

    styling updates header home

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,25 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import {NgForm} from '@angular/forms';
-import {Router} from '@angular/router';
-import {LastFmService} from '../last-fm.service'
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LastFmService } from '../last-fm.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  constructor(public router: Router) {}
 
-  constructor(private router: Router) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  searchForArtist = (form: NgForm)=>{
+  searchForArtist = (form: NgForm) => {
     this.router.navigate(['results'], {
       queryParams: {
-        artist: form.value.artist
-      }
-    })
-  }
+        artist: form.value.artist,
+      },
+    });
+  };
 }
