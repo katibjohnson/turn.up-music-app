a3892563303dd2ad3c065ee5f05755674023b74e
Chris <chrisshaohualiu@gmail.com>
2020-09-22 13:35:52 -0400

    heroku connections

--- src/app/turn-up.service.ts
+++ src/app/turn-up.service.ts
@@ -1,46 +1,50 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TurnUpService {
+  private readonly BASE_URL = environment.apiBaseUrl;
   baseUrl: string = `http://localhost:3000`;
   constructor(private http: HttpClient) {}
 
+
+
   getRecent = (): any => {
-    return this.http.get(`${this.baseUrl}/recent`);
+    return this.http.get(`${this.BASE_URL}/recent`);
   };
 
   getFavoriteArtists = (): any => {
-    return this.http.get(`${this.baseUrl}/favorite-artists`);
+    return this.http.get(`${this.BASE_URL}/favorite-artists`);
   };
 
   getFavoriteVideos = (): any =>{
-    return this.http.get(`${this.baseUrl}/favorite-videos`)
+    return this.http.get(`${this.BASE_URL}/favorite-videos`)
   }
 
   addToRecent = (artist: any): any => {
-    return this.http.post(`${this.baseUrl}/recent`, artist);
+    return this.http.post(`${this.BASE_URL}/recent`, artist);
   };
 
   deleteFromRecent = (id: number): any=>{
-    return this.http.delete(`${this.baseUrl}/recent/${id}`)
+    return this.http.delete(`${this.BASE_URL}/recent/${id}`)
   }
 
   addToFavoriteArtists = (artist: any): any => {
-    return this.http.post(`${this.baseUrl}/favorite-artists`, artist);
+    return this.http.post(`${this.BASE_URL}/favorite-artists`, artist);
   };
 
   deleteFromFavoriteArtists = (id: number): any => {
-    return this.http.delete(`${this.baseUrl}/favorite-artists/${id}`);
+    return this.http.delete(`${this.BASE_URL}/favorite-artists/${id}`);
   };
 
   addToFavoriteVideos = (video: any): any=>{
-    return this.http.post(`${this.baseUrl}/favorite-videos`, video);
+    return this.http.post(`${this.BASE_URL}/favorite-videos`, video);
   }
 
   deleteFromFavoriteVideos = (id: number): any=>{
-    return this.http.delete(`${this.baseUrl}/favorite-videos/${id}`);
+    return this.http.delete(`${this.BASE_URL}/favorite-videos/${id}`);
   }
 }
