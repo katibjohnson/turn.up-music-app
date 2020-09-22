8239a54853aadcefcc5eb6484746073c70f72d72
Kati Johnson <61632071+katibjohnson@users.noreply.github.com>
2020-09-22 17:16:15 -0400

    Revert "main and artist page changes"

--- src/app/last-fm.service.ts
+++ src/app/last-fm.service.ts
@@ -1,38 +1,37 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http'
 import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class LastFmService {
-  constructor(private http: HttpClient, private router: Router) {}
+  
 
-  makeUrl = (method: string): string => {
-    return `https://ws.audioscrobbler.com/2.0/?api_key=c273927229fe0ba2518dfbff80098837&format=json&method=${method}`;
-  };
+  constructor(private http: HttpClient, private router: Router) { }
 
-  getArtists = (searchTerm): any => {
-    return this.http.get(`${this.makeUrl(`artist.search`)}`, {
-      params: { artist: searchTerm },
-    });
-  };
+  makeUrl = (method:string):string=>{
+    return `http://ws.audioscrobbler.com/2.0/?api_key=c273927229fe0ba2518dfbff80098837&format=json&method=${method}`;
+  }
 
-  getTopArtists = (): any => {
+  getArtists=(searchTerm): any=>{
+    return this.http.get(`${this.makeUrl(`artist.search`)}`, {params: {artist: searchTerm}});
+  }
+
+  getTopArtists = (): any=>{
     return this.http.get(`${this.makeUrl(`chart.gettopartists`)}`);
-  };
+  }
 
-  getArtistInfoByMbid = (mbid: string): any => {
+  getArtistInfoByMbid = (mbid: string): any=>{
     return this.http.get(`${this.makeUrl(`artist.getinfo`)}&mbid=${mbid}`);
-  };
+  }
 
-  getArtistInfoByName = (name: string): any => {
+  getArtistInfoByName = (name: string): any=>{
     return this.http.get(`${this.makeUrl(`artist.getinfo`)}&artist=${name}`);
-  };
+  }
 
-  getArtistTopAlbums = (name: string): any => {
-    return this.http.get(
-      `${this.makeUrl(`artist.gettopalbums`)}&artist=${name}`
-    );
-  };
+  getArtistTopAlbums = (name: string): any=>{
+    return this.http.get(`${this.makeUrl(`artist.gettopalbums`)}&artist=${name}`)
+  }
+
 }
